35563e4044cbc6479069c60a1ddc808ae0972109
saurav1811 <saurabhmohapatra23@gmail.com>
2025-12-14 00:40:45 +0530

    Saurav | Add day2 code for repeated sequence of digits in number logic- part2 - using right regex pattern

--- advent-of-code-2025/src/main/kotlin/day2/Day2Main.kt
+++ advent-of-code-2025/src/main/kotlin/day2/Day2Main.kt
@@ -2,24 +2,10 @@ package org.ee.aoc25.day2
 
 import java.io.File
 import java.nio.file.Paths
-import kotlin.math.abs
-import kotlin.math.floor
-import kotlin.math.log10
-import kotlin.math.pow
+import java.util.regex.Pattern
 
 fun main() {
     val resultList = mutableListOf<Long>()
-//    traverseThroughRange(IntRange(11, 22), resultList)
-//    traverseThroughRange(IntRange(95, 115), resultList)
-//    traverseThroughRange(IntRange(998, 1012), resultList)
-//    traverseThroughRange(IntRange(1188511880, 1188511890), resultList)
-//    traverseThroughRange(IntRange(222220, 222224), resultList)
-//    traverseThroughRange(IntRange(1698522, 1698528), resultList)
-//    traverseThroughRange(IntRange(446443, 446449), resultList)
-//    traverseThroughRange(IntRange(38593856, 38593862), resultList)
-//    traverseThroughRange(IntRange(565653, 565659), resultList)
-//    traverseThroughRange(IntRange(824824821, 824824827), resultList)
-//    traverseThroughRange(IntRange(2121212118, 2121212124), resultList)
 
     val absoluteCurrentPath = Paths.get("").toAbsolutePath().toString()
     val file = File("$absoluteCurrentPath/advent-of-code-2025/src/main/kotlin/day2/test_input.txt")
@@ -44,31 +30,25 @@ fun main() {
 
 private fun traverseThroughRange(range: LongRange, resultList: MutableList<Long>) {
     for (number in range) {
-        // Get number of digits in the number
-        val numOfDigits = digitsInNumber(number)
-
-        // Split the number into two equal halves
-        val (temp1, temp2) = splitNumberToHalves(number, numOfDigits)
-
-        if (temp1 == temp2) {
+        if (patternMatchRepeatedDigitsInNumber(number.toString())) {
             resultList.add(number)
         }
     }
 }
 
-fun digitsInNumber(number: Long): Int {
-    return if (number == 0L) 1 else {
-        floor(log10(abs(number).toDouble())).toInt() + 1
-    }
-}
+private fun patternMatchRepeatedDigitsInNumber(numberStr: String): Boolean {
+    val regex = "^(.+)\\1+$"
+    val p = Pattern.compile(regex)
+    val matcher = p.matcher(numberStr)
+    var matchFound = false
 
-private fun splitNumberToHalves(number: Long, numOfDigits: Int): Pair<Long, Long> {
-    var temp1 = number
-    var temp2 = 0L
-    for (power in 0..numOfDigits / 2 - 1) {
-        val unitDigit = temp1 % 10
-        temp2 += unitDigit * 10.0.pow(power.toDouble()).toInt()
-        temp1 /= 10
+    while (matcher.find()) {
+        println(numberStr + " got repeated: " + matcher.group(1))
+        matchFound = true
     }
-    return Pair(temp1, temp2)
+    if (!matchFound) {
+        println("$numberStr has no repetition")
+    }
+
+    return matchFound
 }
